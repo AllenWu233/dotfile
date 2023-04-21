589a46944f6d992cb1f7c3aa76127bc4eb557f25
charleschetty <cfunction@foxmail.com>
2023-04-21 11:40:45 +0800

    fix bufferline

--- config/nvim/lua/user/ui/bufferline.lua
+++ config/nvim/lua/user/ui/bufferline.lua
@@ -55,7 +55,7 @@ bufferline.setup {
     show_buffer_icons = true,
     show_buffer_close_icons = true,
     show_close_icon = true,
-    show_buffer_default_icon = true,
+    --[[ show_buffer_default_icon = true, ]]
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
