83b841580e217c75a1ca6963ec1aa60ba48a9bc4
charleschetty <cfunction@foxmail.com>
2023-05-12 20:16:08 +0800

    update zsh

--- zsh/file_preview.sh
+++ zsh/file_preview.sh
@@ -1,15 +1,10 @@
 #! /usr/bin/env sh
-#
-# export OMZ=/home/charles/.zsh/omz/
-
 mime=$(file -bL --mime-type "$1")
 category=${mime%%/*}
 if [ -d "$1" ]; then
-    exa -l --no-user --no-time --icons "$1" 2>/dev/null || ls --color=always "$1" 2>/dev/null || ls -G "$1"
+    exa -l --no-user --no-time --icons --no-permissions --no-filesize "$1" 2>/dev/null 
 elif [ "$category" = text ]; then
-    (bat -p --color=always "$1" || cat "$1") 2>/dev/null | head -1000
-# elif [ "$category" = image ]; then
-    # command -v ueberzug 2&>/dev/null && bash $OMZ/lib/img_preview.sh "$1"|| img2txt "$1"
+    (bat -p --style numbers --color=always "$1" ) 2>/dev/null | head -1000
 else 
     echo $1 is a $category file
 fi
