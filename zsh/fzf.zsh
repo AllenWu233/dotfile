1f5ca835ccfe0217852fb7cb02c6e2d0955bd356
charleschetty <cfunction@foxmail.com>
2023-07-16 01:35:25 +0800

    fix fzf-tab options

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,5 +1,6 @@
-## https://github.com/yaocccc/omz
 export RUNEWIDTH_EASTASIAN=0
+# export FZF_PREVIEW_COMMAND='bash /home/charles/.zsh/file_preview.sh {}'
+# export FZF_DEFAULT_OPTS="--height 12 --layout=reverse --history=/home/charles/.zsh/cache/fzfhistory"
 export FZF_DEFAULT_OPTS="--preview 'bash ~/.zsh/file_preview.sh {}' --height 12 --layout=reverse --history=/home/charles/.zsh/cache/fzfhistory"
 export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build,dist,vendor} --type f"
 
@@ -11,6 +12,7 @@ zstyle ':fzf-tab:*' prefix ''
 zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
 zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview '[ "$group" = "process ID" ] && ps --pid=$word -o cmd --no-headers -w -w'
 zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-flags --preview-window=down:3:wrap
+zstyle ':fzf-tab:complete:*:options' fzf-flags --preview-window=down:0:wrap
 zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
 zstyle ':fzf-tab:complete:yay:*' fzf-preview 'yay -Qi $word || yay -Si $word'
 zstyle ':fzf-tab:complete:pacman:*' fzf-preview 'pacman -Qi $word || pacman -Si $word'
@@ -22,3 +24,4 @@ zstyle ':fzf-tab:complete:git-show:*' fzf-preview 'git show --color=always $word
 zstyle ':fzf-tab:complete:git-checkout:*' fzf-preview '[ -f "$realpath" ] && git diff --color=always $word || git log --color=always $word'
 zstyle ':fzf-tab:complete:*:*' fzf-preview 'bash ~/.zsh/file_preview.sh ${(Q)realpath}'
 zstyle ':fzf-tab:complete:*:*' fzf-flags --height=12
+zstyle ':fzf-tab:complete:tldr:argument-1' fzf-preview 'tldr --color always $word'
